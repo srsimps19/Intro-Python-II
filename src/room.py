7bdc67e82e87722d7f845d25f2885f19759c01aa
Sydney Crumley <srsimps19@gmail.com>
2020-01-14 16:24:22 -0600

    added method to room.py

--- src/room.py
+++ src/room.py
@@ -5,3 +5,13 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description 
+        self.n_to = self
+        self.e_to = self
+        self.s_to = self
+        self.w_to = self
+    def enter_room(self, direction):
+        new_room = self.__getattribute__(direction)
+
+        if new_room == self:
+            print("You can't move that direction")
+        return new_room
