16806403792e375db4ecc88c5d214970bd32ab66
Sydney Crumley <srsimps19@gmail.com>
2020-01-15 15:29:02 -0600

    fixed take and drop methods on player. {

--- src/player.py
+++ src/player.py
@@ -6,15 +6,17 @@ class Player:
         self.name = name
         self.room = room 
         self.inventory = inventory
-    def take():
+    def take(self, item):
         if item in self.room.items_list:
             self.room.items_list.remove(item)
             self.inventory.append(item)
+            item.on_take()
         else:
             print("That object is not in this room!")
-    def drop():
+    def drop(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
             self.room.items_list.append(item)
+            item.on_drop()
         else:
             print("You don't have that item in your inventory!")
