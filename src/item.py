2cbfb39a6519fb2c3b526c77de62bb813244d75b
Sydney Crumley <srsimps19@gmail.com>
2020-01-15 14:21:57 -0600

    added on drop method to item

--- src/item.py
+++ src/item.py
@@ -2,5 +2,7 @@ class Item:
     def __init__(self, name, desc):
         self.name = name
         self.desc = desc 
-    def on_take():
-        print(f"You have picked up {self.name}".)
+    def on_take(self):
+        print(f"You have picked up {self.name}.")
+    def on_drop(self):
+        print(f"You have dropped {self.name}.")
