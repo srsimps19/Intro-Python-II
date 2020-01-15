ab7af83fc8f8f4c5f85e34c625bc572e6652e7da
Sydney Crumley <srsimps19@gmail.com>
2020-01-15 14:25:34 -0600

    added inventory to player

--- src/player.py
+++ src/player.py
@@ -2,6 +2,7 @@
 # currently.
 
 class Player:
-    def __init__(self, name, room):
+    def __init__(self, name, room, inventory):
         self.name = name
         self.room = room 
+        self.inventory = inventory
