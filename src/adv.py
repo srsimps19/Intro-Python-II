6699ee537db2532f773e90d641c72deda5e2d138
Sydney Crumley <srsimps19@gmail.com>
2020-01-15 14:20:00 -0600

    added on take method to item

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 from textwrap import fill
 
 # Declare all the rooms
--- src/item.py
+++ src/item.py
@@ -2,3 +2,5 @@ class Item:
     def __init__(self, name, desc):
         self.name = name
         self.desc = desc 
+    def on_take():
+        print(f"You have picked up {self.name}".)
